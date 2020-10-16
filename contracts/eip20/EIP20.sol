b8e477255c9b473ce2f4905b373720b3072d5b48
Auctoritas <Auctoritas@users.noreply.github.com>
2020-10-16 18:54:13 +0300

    Update EIP20.sol
    updated to 0.7.3

--- contracts/eip20/EIP20.sol
+++ contracts/eip20/EIP20.sol
@@ -1,11 +1,6 @@
-/*
-Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-.*/
-
-
-pragma solidity ^0.4.21;
-
-import "./EIP20Interface.sol";
+//Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+pragma solidity ^0.7.3;
+import "https://github.com/Auctoritas/Tokens/blob/master/contracts/eip20/EIP20Interface.sol";
 
 
 contract EIP20 is EIP20Interface {
@@ -23,12 +18,12 @@ contract EIP20 is EIP20Interface {
     uint8 public decimals;                //How many decimals to show.
     string public symbol;                 //An identifier: eg SBX
 
-    function EIP20(
+    constructor (
         uint256 _initialAmount,
-        string _tokenName,
+        string memory _tokenName,
         uint8 _decimalUnits,
-        string _tokenSymbol
-    ) public {
+        string memory _tokenSymbol
+    )  {
         balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
         totalSupply = _initialAmount;                        // Update total supply
         name = _tokenName;                                   // Set the name for display purposes
@@ -36,7 +31,7 @@ contract EIP20 is EIP20Interface {
         symbol = _tokenSymbol;                               // Set the symbol for display purposes
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer (address _to, uint256 _value) public override returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -44,7 +39,7 @@ contract EIP20 is EIP20Interface {
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
         balances[_to] += _value;
@@ -56,17 +51,18 @@ contract EIP20 is EIP20Interface {
         return true;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
+    function balanceOf(address _owner) public override view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
+    function approve(address _spender, uint256 _value) public override returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
         return true;
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 }
+
